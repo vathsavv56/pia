2e80e3c21996b7c1aef6d3c4678bda6465d33ee1
vathsavv56_lin <inavoluvathsav007@gmail.com>
2026-09-18 16:41:04 +0530

    chore: complete initial file setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  return (
+    <div className='bg-dgray h-screen w-full'>
+      <div id="left-nav">
+
+      </div>
+      <div id="client">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+  server :{
+    allowedHosts : ["vathsavv56.local"]
+  }
+})
